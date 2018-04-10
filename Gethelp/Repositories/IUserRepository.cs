c84cbdeb048a3dd59facc54d314438299fc4b9ee
qronin <michal.maron@edu.uekat.pl>
2018-04-10 13:47:55 +0200

    ;o

--- Gethelp/Repositories/IUserRepository.cs
+++ Gethelp/Repositories/IUserRepository.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using Gethelp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace Gethelp.Repositories
 {
-    public class IUserRepository : IRepository
+    public interface IUserRepository : IRepository
     {
+
+        User Get(string email);     
+        User Get(Guid id);
+        void Add(User user);
+        void Update(User user);
+        void Remove(Guid id);
 
 
     }
