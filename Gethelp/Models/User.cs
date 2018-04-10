36ab8c2e53fe48906a1211f6402ca4b963712b77
qronin <michal.maron@edu.uekat.pl>
2018-04-10 12:31:13 +0200

    created user customer and therapist, changed to lumen theme

---
+++ Gethelp/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gethelp.Models
+{
+    public class User
+    {
+        //public string Username { get; private set; }
+        //public string password { get; private set; }
+        //public string salt { get; set; }
+        //Guid userId;
+
+
+
+
+    }
+
+
+
+
+    public interface IUser
+    {
+
+    }
+
+}
